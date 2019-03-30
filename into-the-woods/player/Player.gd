e5b4e6240b9426133397fd7f402bc787b82b1a6b
clabe45 <16855387+clabe45@users.noreply.github.com>
2019-03-29 22:31:56 -0400

    :sparkles: Player now walks up tiles

--- into-the-woods/player/Player.gd
+++ into-the-woods/player/Player.gd
@@ -6,7 +6,13 @@ export(int) var max_speed
 export(int) var jump_height
 export(float) var friction_coeff
 
+# TODO: get actual width with code (definitely)
+export(int) var width
+export(int) var height
+export(float) var snap_dist
+
 var motion = Vector2()
+var jumping = false
 
 func _physics_process(delta):
 	motion.y += gravity
@@ -25,5 +31,14 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_key_pressed(KEY_SPACE):
 			motion.y = jump_height
-			
+			jumping = true
+		else:
+			jumping = false
+	
+	# TODO: uncomment the following code and figure out 
+	# snapping to the ground when walking down hills
+	
+	# # don't snap if jumping (set the snap vector to 0,0)
+	# var below_center = Vector2(width / 2, height + snap_dist) if not jumping else Vector2()
+	# motion = move_and_slide_with_snap(motion, below_center, Vector2.UP)
 	motion = move_and_slide(motion, Vector2.UP)
