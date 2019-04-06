1f9080b031b115daa3c36d12471d1035166f98b3
clabe45 <16855387+clabe45@users.noreply.github.com>
2019-04-06 10:34:15 -0400

    :sparkles: Add corn plants

--- into-the-woods/map/structure/corn/generator/CornGenerator.gd
+++ into-the-woods/map/structure/corn/generator/CornGenerator.gd
@@ -4,7 +4,7 @@ extends StructureGenerator
 # Trees can never overlap, so don't let that haPPEN.
 export(int) var spread
 # noise input factors for location
-export(float) var location_multiplier_exists
+export(float) var location_multiplier
 #export(float) var location_multiplier_type
 # lowest noise that can generate a tree
 export(float) var noise_threshold
@@ -26,47 +26,49 @@ func _ready():
 #	for child in get_children():
 #		max_radius = max(max_radius, child.radius)
 
-# tree_x is the index of the potential tree on the tree grid
+# plant_x is the index of the potential tree on the tree grid
 # 	(can be negative)
-func _get_tree_at(tree_x):
-	var top = terrain.get_top_tile(tree_x * spread)
+func _get_plant_at(plant_x):
+	var top = terrain.get_top_tile(plant_x * spread)
 	if top != dirt and top != grass:
 		return null
-	var exists = state.noise.get_noise_2d(tree_x * location_multiplier_exists, 0)
-	if exists < noise_threshold:
+	var exists = state.noise.get_noise_2d(plant_x * location_multiplier, 0)
+	if exists > noise_threshold:	# spawn where trees aren't (roughly)
 		return null
-	# for now don't proc-gen it but use a template
+	# for now just return the only tree;
+	# 	simplex noise isn't evenly distributed, so it would
+	#	be bad to choose from a list with thattt
 	return get_child(0)
 
-	# Note: the list of trees == the children of CornGenerator
+	# Note: the list of trees == the children of TreeGenerator
 
 func can_generate(x):
-	var closest_tree_x = spread * floor(float(x) / spread + 0.5)
+	var closest_plant_x = spread * floor(float(x) / spread + 0.5)
 	# if stack is processed, then terrain is generated
-	return map.is_stack_processed(closest_tree_x)
+	return map.is_stack_processed(closest_plant_x)
 
 func gen_structure(x):
 	# potential origin (on the tree grid)
-	var closest_tree_tree_x = floor(float(x) / spread + 0.5)
+	var closest_plant_tree_x = floor(float(x) / spread + 0.5)
 	# in tiles
-	var closest_tree_x = spread * closest_tree_tree_x
+	var closest_plant_x = spread * closest_plant_tree_x
 
-	var spawn_tile = terrain.get_top_tile(closest_tree_x)
-	var tree = _get_tree_at(closest_tree_tree_x)
+	var spawn_tile = terrain.get_top_tile(closest_plant_x)
+	var tree = _get_plant_at(closest_plant_tree_x)
 	if not(spawn_tile == dirt or spawn_tile == grass) or tree == null:
 		return
 
-	var base_elevation = terrain.sample_height(closest_tree_x) - 1
+	var base_elevation = terrain.sample_height(closest_plant_x) - 1
 
 	for sample_x in range(-tree.radius, +tree.radius):
 		for sample_y in range(-tree.height, 0):
 			var sample = tree.get_cell(sample_x, sample_y)
 			# if greater than -1
 			if sample > TileMap.INVALID_CELL:
-				var world_x = closest_tree_x + sample_x
+				var world_x = closest_plant_x + sample_x
 				var world_y = base_elevation - sample_y
 				# flip y-axis (TODO it's gross, think about this)
 				map.set_cell(world_x, world_y, sample)
 				emit_signal("structure_tile_generated", world_x, world_y)
 
-	emit_signal("structure_generated", closest_tree_x)
+	emit_signal("structure_generated", closest_plant_x)
---
+++ into-the-woods/map/structure/tree/maps/Tree.gd
@@ -0,0 +1,6 @@
+extends TileMap
+
+# half of width
+export(int) var radius
+# start at zero, ends at height
+export(int) var height
