76bc719e34de61eecee2045df76b65676ec05c3a
clabe45 <16855387+clabe45@users.noreply.github.com>
2019-04-10 16:47:37 -0400

    :fire: Remove global actions, prepare BaseObject for development

--- into-the-woods/object/BaseObject.gd
+++ into-the-woods/object/BaseObject.gd
@@ -2,22 +2,5 @@ extends Node2D
 # class_name may cause circular references in some cases :/
 class_name BaseObject
 
-var layer = null
-var type = -1
-
 func _ready():
-	var constants = get_node("/root/Constants")
-	
-	# for some reason, can't set scale of rigidbody, so do it to all children for now
-	$Sprite.scale = Vector2(constants.tile_size, constants.tile_size)
-	if layer == null or type == -1:
-		print("Must set BaseObject's layer and type immediately")
-	
-	# All layers must have a child called TileSet,
-	#	which is the scene used to export that layer's
-	#	tile set.
-	var tile_node = layer.get_node("TileSet").get_child(type)		
-	$Sprite.texture = tile_node.texture
-	
-	var half = constants.tile_size / 2
-	$CollisionShape.get_shape().extents = Vector2(half, half)
+	pass
